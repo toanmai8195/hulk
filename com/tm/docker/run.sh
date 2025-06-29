81345f6cc962bf724bab5748b928230f02cc9343
toan.mai1 <toan.mai1@mservice.com.vn>
2025-06-29 10:46:56 +0700

    Update benchmark go grpc

--- com/tm/docker/run.sh
+++ com/tm/docker/run.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 set -e
 
+echo "🐳 Running docker-compose prometheus..."
+docker compose up -d prometheus
+
+echo "🐳 Running docker-compose node_exporter..."
+docker compose up -d node_exporter
+
+echo "🐳 Running docker-compose grafana..."
+docker compose up -d grafana
+
 echo "🔥 Building image with Bazel..."
 bazel run //com/tm/go/grpc:grpc_server_docker
 
 echo "🐳 Running docker-compose grpc_server..."
-docker compose up grpc_server
+docker compose up -d grpc_server
